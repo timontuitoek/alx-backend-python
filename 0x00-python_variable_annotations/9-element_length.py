79f14d997b1f2ecdaf51bbcc31353d6d6ae81a17
timontuitoek <tuitoektimon@gmail.com>
2024-03-15 00:44:58 +0300

    Annotate the below function’s parameters and return values with the appropriate types

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""def element_length(lst: List[str]) -> List[Tuple[str, int]]"""
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """
+    Returns a list of tuples where each tuple
+    """
+    return [(i, len(i)) for i in lst]
