b56ce73067a6feba5d8e6c44b37ba9b15abe60c0
timontuitoek <tuitoektimon@gmail.com>
2024-03-15 00:03:22 +0300

    minor changes

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
+"""def add(a: float, b: float) -> float:"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    Returns the sum of the given floats."""
     return a + b
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""def floor(n: float) -> int:"""
 import math
 
 
