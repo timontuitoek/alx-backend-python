848f0802e6a60fa7a54885adc51c0e6ba79af8ae
timontuitoek <tuitoektimon@gmail.com>
2024-03-14 23:06:46 +0300

    Readme file, header files and a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats together and returns the result.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two input numbers.
+    """
+    return a + b
