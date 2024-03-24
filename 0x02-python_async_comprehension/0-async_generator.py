94a8cb2994aea686e5b5c45feb3513cfc0d93645
timontuitoek <tuitoektimon@gmail.com>
2024-03-24 23:26:47 +0300

    minor issues

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,40 +1,14 @@
 #!/usr/bin/env python3
+"""Task 0's module.
+"""
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> List[int]:  # type: ignore
-    """Generate a list of random numbers asynchronously"""
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
+    """Generates a sequence of 10 numbers.
+    """
     for _ in range(10):
-        yield random.randint(0, 10)
-
-
-async def async_comprehension() -> List[int]:
-    """Generate a list of random numbers asynchronously"""
-    return [random.randint(0, 10) for _ in range(10)]
-
-
-async def measure_runtime() -> float:
-    """Iterate over the values yielded by async_generator"""
-    start_time = asyncio.get_event_loop().time()
-
-    # Execute async_comprehension four times in parallel using asyncio.gather
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-
-    # End time measurement
-    end_time = asyncio.get_event_loop().time()
-
-    # Calculate total runtime
-    total_runtime = end_time - start_time
-
-    return total_runtime
-
-
-async def main() -> None:
-    # Print the total runtime returned by measure_runtime
-    print(await measure_runtime())
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+        await asyncio.sleep(1)
+        yield random.random() * 10
